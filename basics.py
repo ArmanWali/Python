1d18ef89873799c25574896080c19ff82883a793
ArmanWali <150829536+ArmanWali@users.noreply.github.com>
2024-05-29 23:26:47 +0500

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,13 @@
+x = 10
+y = 15
+# python outputs
+print("The value of x is {} and the value of y is {}".format(x,y))
+
+# Python inputs
+x = input("Enter the first number: ")
+print("The number you have entered is : ", x)
+print("Data type of number is : ", type(x))     # by default it will take input as string
+
+y = int(input("Enter the second number: "))
+print("The number you have entered is : ", y)
+print("Data type of number is : ", type(y))     # now it will take input as an integer
