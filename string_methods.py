cc61456310ec89761d600b92bcfac23ca5a05788
ArmanWali <150829536+ArmanWali@users.noreply.github.com>
2024-06-10 23:02:52 +0500

    Add files via upload

---
+++ string_methods.py
@@ -0,0 +1,6 @@
+course = 'Python for Beginners'
+print(len(course))  # prints the number of characters in the string
+course.upper()  # uppercase the characters in the string
+course.lower()  # lowercase the characters in the string
+print(course.find('n'))  # prints the index of the character in the string
+course.replace('Beginners', 'Absolute Beginners')  # replacing the word Beginners with "Absolute Beginners"
