236cddb0cf9e5e7a485b8c813b3749774192dbf2
ArmanWali <150829536+ArmanWali@users.noreply.github.com>
2024-06-12 20:12:35 +0500

    Add files via upload

---
+++ List_Methods.py
@@ -0,0 +1,19 @@
+numbers = [5, 2, 1, 7, 4]
+
+numbers.append(20)    # this will add '20' at the end of the list
+
+numbers.insert(0,30)  # this will add '30' at the index of 0 and push all the values to the right
+
+numbers.remove(2)     # this will remove '2' from the list
+
+numbers.pop()         # removing the last value from the list
+
+numbers.index(5)      # returns the index of 5
+
+numbers.sort()        # for sorting the list
+
+numbers.reverse()     # for reversing the list
+
+numbers2 = numbers.copy()   # this will copy the numbers list in number2 list
+
+numbers.clear()       # this will empty the list
