c288f8c723d81a8e20cfaf41bac9ab6b023789fb
ArmanWali <150829536+ArmanWali@users.noreply.github.com>
2024-06-17 00:40:37 +0500

    Update problem_07.py

--- problem_07.py
+++ problem_07.py
@@ -1,3 +1,13 @@
 def are_anagrams(str1, str2):
-    if len(str1) == len(str2):
-        
+    s1 = (str1.replace(" ","")).lower()   # it will remove spaces and lowercase the string
+    s2 = (str2.replace(" ","")).lower()
+    if sorted(s1) == sorted(s2):          
+        print("They are anagrams of each other")
+    else: 
+        print("They are not anagrams of each other")
+
+
+string1 = input("Please enter the first string : ")
+string2 = input("Please enter the second string : ")
+
+are_anagrams(string1, string2)
