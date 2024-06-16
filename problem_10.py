739d9041d7c8e5ba133d3ce170e8e3412ce3a1f0
ArmanWali <150829536+ArmanWali@users.noreply.github.com>
2024-06-17 00:00:01 +0500

    Update problem_10.py

--- problem_10.py
+++ problem_10.py
@@ -1,6 +1,6 @@
 def reverse_string(str):
     str2 = ''
-    for i in range(len(str)-1,-1,-1):
+    for i in range(len(str)-1,-1,-1): # for running reverse loop
         str2 += str[i]
     return str2
 
