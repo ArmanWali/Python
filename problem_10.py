0d9aac2ebbb46f1e1aea533963d0cfbe572dc8fd
ArmanWali <150829536+ArmanWali@users.noreply.github.com>
2024-06-16 23:59:22 +0500

    Update problem_10.py

---
+++ problem_10.py
@@ -0,0 +1,9 @@
+def reverse_string(str):
+    str2 = ''
+    for i in range(len(str)-1,-1,-1):
+        str2 += str[i]
+    return str2
+
+
+string = input("Please enter any string : ")
+print('The reverse of the string is :', reverse_string(string))
